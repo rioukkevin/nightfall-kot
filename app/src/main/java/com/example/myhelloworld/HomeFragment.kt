3c624015d1a11cb81cdfb1256b591f5efacb2165
Dimitri SAUVAGE <dimitri1993@live.fr>
2020-10-15 14:22:59 +0200

    Add QR Code scan

--- app/src/main/java/com/example/myhelloworld/HomeFragment.kt
+++ app/src/main/java/com/example/myhelloworld/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.myhelloworld
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.fondesa.kpermissions.allGranted
+import com.fondesa.kpermissions.extension.permissionsBuilder
+import com.fondesa.kpermissions.extension.send
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -12,7 +17,19 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home,container,false)
+        //Ask for permission
+        permissionsBuilder(Manifest.permission.CAMERA).build().send { result ->
+            // Handle the result, for example check if all the requested permissions are granted.
+            if (!result.allGranted()) {
+                // All the permissions are granted.
+                Toast.makeText(
+                    requireActivity(),
+                    "Vous n'avez pas accepté les droits, le QR Code ne fonctionnera pas",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+        return inflater.inflate(R.layout.fragment_home, container, false)
 
     }
 }
